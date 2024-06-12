8746fc2745e4335d1f0f8b51d166b18003cf3733
deshrajaristocrat <132996665+deshrajaristocrat@users.noreply.github.com>
2024-06-12 18:55:22 +0530

    Update Program.cs
    Signed-off-by: deshrajaristocrat <132996665+deshrajaristocrat@users.noreply.github.com>

--- Program.cs
+++ Program.cs
@@ -9,11 +9,12 @@ namespace C_Program
      class Program
     {
         static void Main(string[] args)
-        {
+        {!!!
             string message = "Hello World!!";
             For(i=0;i<10;i++);
              {
-                  
+               abcd;
+                  bcdef;
              }
             Console.WriteLine(message);
             Console.ReadLine();
